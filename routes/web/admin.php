8db955bd93d8991aa4e920a7df2cd18638f9e176
Akachukwu Aneke <akalodave@gmail.com>
2026-01-20 15:07:51 +0100

    fixed bugs

--- routes/web/admin.php
+++ routes/web/admin.php
@@ -4,13 +4,22 @@ use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
 
 Route::middleware('guest:web')->group(function () {
-    Route::get('login', [AuthController::class, 'login'])->name('login');
-    Route::post('login', [AuthController::class, 'authenticate'])->name('login.store');
+    Route::get('login', [AuthController::class, 'login'])->name('admin.login');
+    Route::post('login', [AuthController::class, 'authenticate'])->name('admin.login.store');
 });
 
 Route::middleware('auth:web')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::resource('users', DashboardController::class)->only(['index']);
-    Route::resource('services', DashboardController::class)->only(['index']);
+    Route::post('logout', [AuthController::class, 'logout'])->name('admin.logout');
+    Route::get('', [DashboardController::class, 'index'])->name('admin.index');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    // Route::get('users', DashboardController::class)->only(['index']);
+    // Route::resource('services', DashboardController::class)->only(['index']);
+
+    Route::group(['prefix' => '/users'], function () {
+        Route::get('', [DashboardController::class, 'users'])->name('admin.users.index');
+    });
+
+    Route::group(['prefix' => '/services'], function () {
+        Route::get('', [DashboardController::class, 'services'])->name('admin.services.index');
+    });
 });
