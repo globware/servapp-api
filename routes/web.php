aaf525c5def27e9ee5ca6f8883a223140eec1097
Akachukwu Aneke <akalodave@gmail.com>
2026-02-11 06:27:54 +0100

    debug

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Middleware\LogRequestUrl;
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::domain(config('app.admin_domain'))->group(function () {
+Route::domain(config('app.admin_domain'))->middleware(LogRequestUrl::class)->group(function () {
     // Route::prefix('admin')->name('admin.')->group(function () {
     require __DIR__ . '/web/admin.php';
 });
