ba5d5a6d5bda0c272a31ae7b27c69a841e670414
Akachukwu Aneke <akalodave@gmail.com>
2026-02-11 06:40:18 +0100

    debug

--- public/index.php
+++ public/index.php
@@ -13,19 +13,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-$request = Request::capture();
-dd([
-    'full_url' => $request->fullUrl(),
-    'url' => $request->url(),
-    'path' => $request->path(),
-    'method' => $request->method(),
-    'host' => $request->getHost(),
-    'scheme' => $request->getScheme(),
-    'server_name' => $_SERVER['SERVER_NAME'] ?? 'N/A',
-    'http_host' => $_SERVER['HTTP_HOST'] ?? 'N/A',
-    'request_uri' => $_SERVER['REQUEST_URI'] ?? 'N/A',
-]);
-
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
