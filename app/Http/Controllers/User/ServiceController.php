a9b8dd290fe79e4274b7405b0ed4f8d848258703
Akachukwu Aneke <akalodave@gmail.com>
2026-06-07 17:55:03 +0100

    fix: update userServices relationship key in ServiceController to match model definition

--- app/Http/Controllers/User/ServiceController.php
+++ app/Http/Controllers/User/ServiceController.php
@@ -50,7 +50,7 @@ class ServiceController extends Controller
         $this->serviceService->limit = 5;
         $this->serviceService->approved = true;
         $services = collect([]);
-        $services = $this->serviceService->getServices(['userService' => ['service', 'media', 'tags', 'feedbacks']]);
+        $services = $this->serviceService->getServices(['userServices' => ['service', 'media', 'tags', 'feedbacks']]);
 
         return Utilities::ok(ServiceResource::collection($services));
         
