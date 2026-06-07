771e9a1e5f90fe4000fb4b22133c063a91e73ca2
Akachukwu Aneke <akalodave@gmail.com>
2026-06-07 18:04:39 +0100

    refactor: optimize service relationship loading by moving eager loads into getService query constraints

--- app/Http/Controllers/User/ServiceController.php
+++ app/Http/Controllers/User/ServiceController.php
@@ -50,7 +50,7 @@ class ServiceController extends Controller
         $this->serviceService->limit = 5;
         $this->serviceService->approved = true;
         $services = collect([]);
-        $services = $this->serviceService->getServices(['userServices.service', 'userServices.media', 'userServices.tags', 'userServices.feedbacks']);
+        $services = $this->serviceService->getServices(['userServices']);
 
         return Utilities::ok(ServiceResource::collection($services));
         
@@ -82,7 +82,9 @@ class ServiceController extends Controller
 
     public function getService(Request $request, $serviceId)
     {
-        $service = $this->serviceService->getService($serviceId, ['userServices' => fn($query) => $query->where('approved', true)]);
+        $service = $this->serviceService->getService($serviceId, [
+            'userServices' => fn($query) => $query->where('approved', true)->with(['service', 'media', 'tags', 'feedbacks'])
+        ]);
         if(!$service) return Utilities::error402("Service not found");
 
         return Utilities::ok(new ServiceResource($service));
